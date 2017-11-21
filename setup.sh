d40c6984c8ee52b401f0cdd073ec453d70908e8a
Ryan O'Connor <charle692@gmail.com>
2017-11-20 23:24:18 -0500

    Uncommented setup configuration code. It works!

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ clear
 
 echo "Configuring the Raspberry Pi"
 rm ./tmp/*
-# sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
-# sudo cp ./configuration\ files/dhcpd.conf /etc/dhcp/
-# sudo cp ./configuration\ files/hostapd.conf /etc/hostapd/
-# sudo cp ./configuration\ files/interfaces.aphost /etc/network/interfaces
-# sudo cp ./configuration\ files/isc-dhcp-server.aphost /etc/default/isc-dhcp-server
-# sudo cp ./configuration\ files/rc.local.aphost /etc/rc.local
+sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp ./configuration\ files/dhcpd.conf /etc/dhcp/
+sudo cp ./configuration\ files/hostapd.conf /etc/hostapd/
+sudo cp ./configuration\ files/interfaces.aphost /etc/network/interfaces
+sudo cp ./configuration\ files/isc-dhcp-server.aphost /etc/default/isc-dhcp-server
+sudo cp ./configuration\ files/rc.local.aphost /etc/rc.local
 clear
 echo "Configuration completed"
 
